e4b45b49b33281378374675d3723066221288f0b
HaeJin Lee <seareale@gmail.com>
2023-03-07 13:12:59 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,18 +8,16 @@ with open(README_path, encoding='utf-8') as f:
     long_description = f.read()
 setup(
     name='randaugment',
-    version='1.0.2',
+    version='0.0.1',
     packages=find_packages(),
-    url='https://github.com/jizongFox/pytorch-randaugment',
+    url='https://github.com/seareale/pytorch-randaugment',
     license='MIT',
-    author='Jizong Peng',
-    author_email='jizong.peng.1@etsmtl.net',
     long_description=long_description,
     long_description_content_type='text/markdown',
     install_requires=[],
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.10",
     ],
 )
